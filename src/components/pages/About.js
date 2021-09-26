6246714e77d1238adafa529b31ea521a37bc7f30
Abir s <abir-oz>
2021-09-26 19:06:36 +0600

    first commit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,14 @@ const About = () => {
     <Fragment>
       <h1>About This App</h1>
       <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
+      <p>
+        GitHub Profile Finder Using React and GitHub User API API Endpoints that
+        has been used: https://api.github.com/users
+        https://api.github.com/search/users?q=search_term
+        https://api.github.com/users/username
+        https://api.github.com/users/username/repos Components - App - pages -
+        layout - repos - users Pages - Home - About - Not Found
+      </p>
     </Fragment>
   );
 };
